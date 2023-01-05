b25672449feea9c232fdf3e46d56f27ba40af9f9
monalisa <monalisaganguly620@gmail.com>
2023-01-05 12:24:31 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const[counter,SetCounter] = useState(0)
+  const[decr, SetDecr] = useState(100)
+  return(
+    <div>
+      <p onClick={()=>SetCounter(counter+1)}>This is a counter{counter}</p>
+      <p onClick={()=>SetDecr(decr-1)}>This is a counter{decr}</p>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
